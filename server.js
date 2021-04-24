e862bc591ddd461dfe5dfd8f79825d18da5b2712
anita-akopyan <83118222+anita-akopyan@users.noreply.github.com>
2021-04-24 14:29:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { MongoClient } = require("mongodb");
 const ObjectId = require('mongodb').ObjectId; 
 
 const uri =
-  "mongodb+srv://cse120-2021-user:aua-gened-cse120@cse120-2021.zmhgf.mongodb.net/test";
+ "mongodb+srv://anita_akopyan:samanthaablessing7@cluster0.keijd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
